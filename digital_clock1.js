28a47421b03c2a21659f1d48af137f9e5faa63cc
OduriJayaDeepika <deepikaoduri@gmail.com>
2024-07-16 10:15:28 +0530

    first commit

---
+++ digital_clock1.js
@@ -0,0 +1,10 @@
+function time(){
+    var x = new Date();
+    document.getElementsByClassName("hours")[0].innerHTML = x.getHours();
+    document.getElementsByClassName("minutes")[0].innerHTML = x.getMinutes();
+    document.getElementsByClassName("seconds")[0].innerHTML = x.getSeconds();
+    if(x.getSeconds() == 10){
+        clearInterval(n);
+    }
+    }
+    var n = setInterval(time,1000);
